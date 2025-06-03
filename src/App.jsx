d105e307b8ee62f00c95755a3aa15b4e8868afbc
SHANGAMESH D.K <shangameshdkss@gmail.com>
2025-06-03 17:55:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,21 @@ function App() {
     window.location.href = "https://shangamesh.netlify.app/1/#about";
   };
   useEffect(() => {
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const isDesktop = !isMobile;
 
-    const isWrongDomain = window.location.hostname === "shangameshdk.netlify.app";
+  const isWrongDomain = window.location.hostname === "mshangamesh.netlify.app";
 
-    if (isMobile && isWrongDomain) {
-     window.location.href = "https://shangamesh.netlify.app"; 
-    }
-  }, []);
+  if (isDesktop && isWrongDomain) {
+    window.location.href = "https://shangameshdk.netlify.app";
+  }
+}, []);
   
   useGSAP(() => {
     const tl = gsap.timeline();
 
     tl.to(".vi-mask-group", {
-      rotate: 10,
+      rotate: 20,
       duration: 2,
       ease: "Power4.easeInOut",
       transformOrigin: "50% 50%",
@@ -100,7 +101,7 @@ function App() {
   return (
     <>
       <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
-        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+        <svg viewBox="0 0 800 2000" preserveAspectRatio="xMidYMid slice">
           <defs>
             <mask id="viMask">
               <rect width="100%" height="100%" fill="black" />
@@ -129,14 +130,14 @@ function App() {
         </svg>
       </div>
       {showContent && (
-        <div className="main w-full rotate-[-10deg] scale-[1.7]">
+        <div className="main w-full  scale-[1]">
           <div className="landing overflow-hidden relative w-full h-screen bg-black">
-            <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10">
-              <div className="logo flex gap-7">
+            <div className="navbar absolute top-0 left-0 z-[10] w-full py-12 px-10">
+              <div className="logo flex gap-4">
                 <div className="lines flex flex-col gap-[5px]">
-                  <div className="line w-15 h-2 bg-white"></div>
-                  <div className="line w-8 h-2 bg-white"></div>
-                  <div className="line w-5 h-2 bg-white"></div>
+                  <div className="line w-10 h-1.5 bg-white"></div>
+                  <div className="line w-7 h-1.5 bg-white"></div>
+                  <div className="line w-4 h-1.5 bg-white"></div>
                 </div>
                 <h3 className="text-4xl -mt-[8px] leading-none text-white">
                   SHANGAMESH D K
@@ -151,13 +152,13 @@ function App() {
                 src="./bg.jpeg"
                 alt=""
               />
-              <div className="text text-white flex flex-col gap-3 absolute top-40 left-2/5 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
-                <h1 className="text-[12rem] leading-none -ml-40">MY</h1>
-                <h1 className="text-[12rem] leading-none ml-20">PORTFOLIO</h1>
-                <h1 className="text-[12rem] leading-none -ml-40">WEBSITE</h1>
+              <div className="text text-white flex flex-col gap-3 absolute top-60 left-4/9 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
+                <h1 className="text-[4rem] leading-none -ml-0">MY</h1>
+                <h1 className="text-[4rem] leading-none ml-20">PORTFOLIO</h1>
+                <h1 className="text-[4rem] leading-none -ml-0">WEBSITE</h1>
               </div>
               <img
-                className="absolute character -bottom-[150%] left-2/10 -translate-x-1/2  scale-[1] rotate-[-20deg]"
+                className="absolute character top-100 left-2/10 -translate-x-1/2  scale-[1] rotate-[-20deg]"
                 src="./shang.png"
                 alt=""
               />
@@ -175,18 +176,16 @@ function App() {
           <div className="line w-1 h-1 bg-white"></div>
           <div className="w-full h-screen flex items-center justify-center bg-black">
             <div className="cntnr flex text-white w-full h-[80%] ">
-              <div className="limg relative w-1/2 h-full">
-                <img
-                  className="absolute scale-[1] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              <img
+                  className="absolute scale-[65%] bottom-10 left-1/6"
                   src="./imag.png"
                   alt=""
                 />
-              </div>
-              <div className="rg w-[60%] py-0">
+              <div className="px-10">
                 <div style={{ fontFamily: 'Space Grotesk' }}>
-                  <h1 className="text-7xl font-bold">Hi,</h1>
-                  <h2 className="text-6xl font-bold">I am Shangamesh D K,</h2>
-                  <h1 className="mt-2 text-[3rem]  font-bold">Software Developer
+                  <h1 className="text-4xl font-bold">Hi,</h1>
+                  <h2 className="text-4xl font-bold">I am Shangamesh D K,</h2>
+                  <h1 className="mt-2 text-2xl  font-bold">Software Developer
 & VFX Artist.</h1>
                 </div>
                 <div
@@ -194,31 +193,18 @@ function App() {
                   style={{ backgroundColor: "#1FA574" }}
                 ></div>
                 <div style={{ fontFamily: "Space Grotesk" }}>
-                  <p className="mt-10 text-xl font-bold opacity-80">
+                  <p className="mt-10  font-bold opacity-80">
                   I’m a passionate and results-driven Software Developer and VFX Artist with a keen eye for detail and a strong commitment to innovation. Whether it’s building seamless web applications, crafting immersive visual experiences, or optimizing performance, I take pride in delivering efficient, scalable, and visually compelling solutions.
                   </p>
                 </div>
-                <div style={{ fontFamily: "Space Grotesk" }}>
-                  <p className="mt-5 text-xl font-bold opacity-80">
-                  With a background that bridges both technical development and digital creativity, I bring a unique perspective to every project. My experience ranges from front-end and back-end development to 3D design, compositing, and animation, enabling me to approach challenges from both functional and aesthetic angles.
-                  </p>
-                </div>
-                <div style={{ fontFamily: "Space Grotesk" }}>
-                  <p className="mt-5 text-xl font-bold opacity-80">
-                  I believe that technology should be intuitive and impactful. My goal is always to create high-quality, user-centric outcomes that align with client goals and enhance user experiences. I’m constantly exploring new tools, trends, and techniques to stay ahead in the ever-evolving world of software and visual effects.
-                  </p>
-                </div><div style={{ fontFamily: "Space Grotesk" }}>
-                  <p className="mt-10 text-xl font-bold opacity-80">
-                  💡 Let’s collaborate and turn your vision into a reality—whether it’s a cutting-edge application, an engaging portfolio, or a stunning visual production.
-                  </p>
-                </div>  
+                 
                 <button
                   onClick={handleRedirect}
-                  className="mt-10 px-6 py-3 bg-[#1FA574] text-white font-bold text-xl rounded-lg hover:bg-green-700 transition-all"
+                  className="mt-20  px-4 py-3 bg-[#1FA574] text-white font-bold text-xl rounded-lg hover:bg-green-700 transition-all"
                 >
                   KNOW MORE
                 </button>
-
+                
               </div>
             </div>
           </div>
